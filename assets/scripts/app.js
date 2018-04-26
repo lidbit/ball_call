7e61524a98458396fd91e1bd2a8adc9376a0958f
lidbit <lydia.d.1001@gmail.com>
2018-04-26 23:17:36 +1000

    Color picker wip.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -11,45 +11,63 @@ $(document).ready(function() {
 
 //initialising document
 function init() {
-    // hide and show sections
+    var data = {
+        fancyDiv: {
+            backgroundColor: 'lightgrey',
+            borderRadius: '10px',
+            padding: '10px',
+            marginTop: '30px'
+        },
+        fancierDiv: {
+            fontFamily: 'Arial, Helvetica',
+            border: 'solid 1px black',
+            padding: '30px'
+        },
+
+        cold: true,
+        hot: false,
+        cssClasses: {
+            square: true,
+            'dashed-border': true,
+            blue: false
+        },
+        squareClass: 'square',
+        bgColorClass: 'red',
+        selected: true,
+
+        expcircleClass: 'expcircle',
+
+        circleRadius: 200,
+        radiusNumber: 130,
+        sizeNumber: 260,
+        satNumber: 50,
+        hueNumber: 130,
+        lightNumber: 50
+
+    };
+    // slider component wip
+    Vue.component('button-counter', {
+        data: function() {
+            return {
+                count: 0,
+                items: [{
+                        message: "one"
+                    },
+                    {
+                        message: "two"
+                    },
+                    {
+                        message: "three"
+                    },
+                ]
+            }
+        },
+        template: '<div id="huetable"><table class="tableslider"><tbody><tr v-for="item in items"><td>{{ item.message }}</td></tr></div>'
+            // template: '<button v-on:click="count++">You clicked me {{ count }} times.</button>'
+    });
 
     new Vue({
         el: '#app',
-        data: {
-
-            fancyDiv: {
-                backgroundColor: 'lightgrey',
-                borderRadius: '10px',
-                padding: '10px',
-                marginTop: '30px'
-            },
-            fancierDiv: {
-                fontFamily: 'Arial, Helvetica',
-                border: 'solid 1px black',
-                padding: '30px'
-            },
-
-            cold: true,
-            hot: false,
-            cssClasses: {
-                square: true,
-                'dashed-border': true,
-                blue: false
-            },
-            squareClass: 'square',
-            bgColorClass: 'red',
-            selected: true,
-
-            expcircleClass: 'expcircle',
-
-            circleRadius: 200,
-            radiusNumber: 130,
-            sizeNumber: 260,
-            satNumber: 50,
-            hueNumber: 130,
-            lightNumber: 50
-
-        }
+        data: data
     });
-
 }
